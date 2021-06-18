e88779f6310df274b2267b0acec86685250f1679
ali <ali.mahmoodi.heris@gmail.com>
2021-06-18 20:46:07 +0300

    implement business layer

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetById(int productId);
+        List<Product> GetList();
+        List<Product> GetListByCategoryId(int categoryId);
+        Product Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using DataAccsess.Abstract;
+using DataAccsess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProdcutDal _prodcutDal;
+
+        public ProductManager(IProdcutDal prodcutDal)
+        {
+            _prodcutDal = prodcutDal;
+        }
+
+        public Product Add(Product product)
+        {
+            return _prodcutDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _prodcutDal.Delete(product);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _prodcutDal.Get(p => p.ProductID == productId);
+        }
+
+        public List<Product> GetList()
+        {
+            return _prodcutDal.GetList().ToList();
+        }
+
+        public List<Product> GetListByCategoryId(int categoryId)
+        {
+            return _prodcutDal.GetList(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            _prodcutDal.Update(product);
+        }
+    }
+}
